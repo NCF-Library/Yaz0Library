94314f507501cc34686c1087312db7a796ec5a5b
ArchLeaders <archparliament@gmail.com>
2023-01-09 18:46:28 -0800

    Add de/compression benchmarks

---
+++ tests/CompressBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Yaz0Library;
+
+namespace Yaz0Tests
+{
+    [MemoryDiagnoser]
+    public class CompressBenchmark
+    {
+        private const string Path = "D:\\Bin\\Yaz0Library\\Enemy_Lynel_Dark.bactorpack";
+
+        [Benchmark]
+        public byte[] Yaz0Impl()
+        {
+            return Yaz0.CompressUnmanaged(Path);
+        }
+
+        [Benchmark]
+        public byte[] IlImpl()
+        {
+            return Yaz0.CompressManaged(Path);
+        }
+
+        [Benchmark]
+        public ReadOnlySpan<byte> CeadImpl()
+        {
+            return Yaz0.Compress(File.ReadAllBytes(Path));
+        }
+    }
+}
---
+++ tests/DecompressBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Yaz0Library;
+
+namespace Yaz0Tests
+{
+    [MemoryDiagnoser]
+    public class DecompressBenchmark
+    {
+        private const string Path = "D:\\Bin\\Yaz0Library\\Enemy_Lynel_Dark.sbactorpack";
+
+        [Benchmark]
+        public byte[] Yaz0Impl()
+        {
+            return Yaz0.DecompressUnmanaged(Path);
+        }
+
+        [Benchmark]
+        public byte[] IlImpl()
+        {
+            return Yaz0.DecompressManaged(Path);
+        }
+
+        [Benchmark]
+        public Span<byte> CeadImpl()
+        {
+            return Yaz0.Decompress(File.ReadAllBytes(Path));
+        }
+    }
+}
