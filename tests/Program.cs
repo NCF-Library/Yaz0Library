6c57fe8244c254b629419008c39c09cdb21b255a
ArchLeaders <archparliament@gmail.com>
2023-01-09 18:48:51 -0800

    Simple debug tests

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,4 +1,15 @@
 ﻿using Yaz0Library;
 
-Yaz0.DecompressFast("D:\\Bin\\Yaz0Library\\Enemy_Lynel_Dark.sbactorpack");
-Yaz0.CompressFast("D:\\Bin\\Yaz0Library\\Enemy_Lynel_Dark.bactorpack");
+string c1Path = "D:\\Bin\\Yaz0Library\\Enemy_Lynel_Dark.bactorpack";
+string c2Path = "D:\\Bin\\Yaz0Library\\Enemy_Bokoblin_Dark.bactorpack";
+
+var c1 = Yaz0.Compress(File.ReadAllBytes(c1Path));
+var c2 = Yaz0.Compress(File.ReadAllBytes(c2Path));
+
+using (FileStream writer = File.Create("D:\\c1.sbactorpack")) {
+    writer.Write(c1);
+}
+
+using (FileStream writer = File.Create("D:\\c2.sbactorpack")) {
+    writer.Write(c2);
+}
