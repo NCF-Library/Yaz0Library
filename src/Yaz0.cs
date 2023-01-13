efc258c79565f4f73d996092cf53c67b3eed9eb6
ArchLeaders <archparliament@gmail.com>
2023-01-13 12:16:52 -0800

    Pass handle as out argument

--- src/Yaz0.cs
+++ src/Yaz0.cs
@@ -6,7 +6,7 @@ namespace Yaz0Library
     public static partial class Yaz0
     {
         [LibraryImport("Cead.dll")]
-        internal static unsafe partial void Compress(byte* src, int src_len, out VectorSafeHandle dst_handle, out byte* dst, out int dst_len, uint data_alignment, int level);
+        internal static unsafe partial void Compress(byte* src, int src_len, out Yaz0SafeHandle dst_handle, out byte* dst, out int dst_len, uint data_alignment, int level);
 
         [LibraryImport("Cead.dll")]
         internal static unsafe partial void Decompress(byte* src, int src_len, byte* dst, int dst_len);
@@ -15,13 +15,13 @@ namespace Yaz0Library
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static partial bool FreeResource(IntPtr vector_ptr);
 
-        public static unsafe Span<byte> Compress(string file, int level = 7) => Compress(File.ReadAllBytes(file), level);
-        public static unsafe Span<byte> Compress(ReadOnlySpan<byte> src, int level = 7)
+        public static unsafe Span<byte> Compress(string file, out Yaz0SafeHandle handle, int level = 7) => Compress(File.ReadAllBytes(file), out handle, level);
+        public static unsafe Span<byte> Compress(ReadOnlySpan<byte> src, out Yaz0SafeHandle handle, int level = 7)
         {
             Yaz0Helper.LoadDlls();
 
             fixed (byte* srcPtr = src) {
-                Compress(srcPtr, src.Length, out VectorSafeHandle dstHandle, out byte* dst, out int dstLen, 0, level);
+                Compress(srcPtr, src.Length, out handle, out byte* dst, out int dstLen, 0, level);
                 return new(dst, dstLen);
             }
         }
