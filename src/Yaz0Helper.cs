876052d501464fb95793fe79af1e203d706c2cc2
ArchLeaders <archparliament@gmail.com>
2023-01-13 13:37:23 -0800

    Make DllLoader publicly accessible to support pre-loading for parallel usage

--- src/Yaz0Helper.cs
+++ src/Yaz0Helper.cs
@@ -11,31 +11,28 @@ namespace Yaz0Library
         [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
         internal static extern IntPtr SetDllDirectory(string lpFileName);
 
-        internal static void LoadDlls()
+        public static void LoadDlls()
         {
             if (IsLoaded) {
                 return;
             }
 
             string path = Path.Combine(Path.GetTempPath(), $"Yaz0Library-{typeof(Yaz0).Assembly.GetName().Version}");
-
-            foreach (var lib in Libs) {
-                string dll = Path.Combine(path, lib);
-                // Always copy in debug mode
+            string dll = Path.Combine(path, "Cead.dll");
+            // Always copy in debug mode
 #if DEBUG
+            Directory.CreateDirectory(path);
+            using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"Yaz0Library.Lib.Cead.dll");
+            using FileStream fs = File.Create(dll);
+            stream.CopyTo(fs);
+#else
+            if (!File.Exists(dll)) {
                 Directory.CreateDirectory(path);
-                using Stream stream = Assembly.GetCallingAssembly().GetManifestResourceStream($"Yaz0Library.Lib.Cead.dll");
+                using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"Yaz0Library.Lib.Cead.dll");
                 using FileStream fs = File.Create(dll);
                 stream.CopyTo(fs);
-#else
-                if (!File.Exists(dll)) {
-                    Directory.CreateDirectory(path);
-                    using Stream stream = Assembly.GetCallingAssembly().GetManifestResourceStream($"Yaz0Library.Lib.Cead.dll");
-                    using FileStream fs = File.Create(dll);
-                    stream.CopyTo(fs);
-                }
-#endif
             }
+#endif
 
             SetDllDirectory(path);
             IsLoaded = true;
